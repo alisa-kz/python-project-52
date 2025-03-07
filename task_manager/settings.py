7d5d899cec0d2f179d5a7b5456778347687933d8
Alisa Kuzyagina <khairutdinovaaalisa@gmail.com>
2025-03-07 14:51:56 +0300

    update settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -101,16 +101,22 @@ WSGI_APPLICATION = "task_manager.wsgi.application"
 #         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
-database_url = os.getenv("DATABASE_URL")
+database_url = os.getenv("DATABASE_URL", default=None)
 
 if database_url:
-    DATABASES = {"default": dj_database_url.parse(database_url)}
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=database_url,
+            conn_max_age=600,
+        )
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
 # db_from_env = dj_database_url.config(conn_max_age=600)
 # DATABASES['default'].update(db_from_env)
 
